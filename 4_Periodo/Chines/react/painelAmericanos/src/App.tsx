4655727bb828eaff68d2001b236f7858559cab54
joaopedro <joaopkrodrigues@gmail.com>
2024-08-09 20:02:36 -0300

    Adicionando arquivos da aula 09/08/24 - React

---
+++ 4_Periodo/Chines/react/painelAmericanos/src/App.tsx
@@ -0,0 +1,15 @@
+import { Rotas } from "./routes"
+
+function App() {
+
+  return (
+  <>
+    <Rotas />
+  </>
+  )
+}
+
+//mobiel
+const stuyles2 = StyleSheet
+
+export default App
---
+++ 4_Periodo/Chines/react/painelAmericanos/src/components/NovuComponent/index.tsx
@@ -0,0 +1,5 @@
+export const NovuComponent = () => {
+    return(
+        <h1>Novu component</h1>
+    )
+} 
